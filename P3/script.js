fda4132e6bd9e15acef368d351feb401eccbfc3a
Germán Castro <germancastromontes@gmail.com>
2019-05-14 23:53:42 +0200

    botón cargar mas eventos

--- P3/script.js
+++ P3/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-$('.navbar-light .dmenu').hover(function () {
-        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-    }, function () {
-        $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
-    });
+    $('.navbar-light .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+    $('#Carousel').carousel({
+        interval: 5000
+    })
 });
+
